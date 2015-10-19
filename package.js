def899b2094230b3c045432905f7f3ff78a4e408
Alex Beauchemin <abeauchemin@u92.com>
2015-10-19 14:07:14 -0400

    Better button style

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'alexbeauchemin:accounts-twitch',
-  version: '1.0.1',
+  version: '1.0.2',
   summary: 'A login service for Twitch',
   git: 'https://github.com/AlexBeauchemin/meteor-accounts-twitch',
   documentation: 'README.md'
